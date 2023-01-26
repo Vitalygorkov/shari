14b0e8d433fa08206da7deabd2b16ef6a67a8252
Vitalygorkov <vet55omsk@gmail.com>
2023-01-26 22:11:56 +0600

    add template and img css

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Category, Product
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = ['1']
+    template = loader.get_template('shop/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render( context, request))
