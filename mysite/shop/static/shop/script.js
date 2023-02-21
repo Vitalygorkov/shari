4054b877711f3257d106737e26bdc2e4283b376f
Alenadecor <alenagorcova@gmail.com>
2023-02-21 23:12:50 +0600

    pravki20230221

--- mysite/shop/static/shop/script.js
+++ mysite/shop/static/shop/script.js
@@ -3,41 +3,54 @@ const isMobile = {
     Android: function () {
         return navigator.userAgent.match(/Android/i);
     },
-    BlackBerry: function () {
+    BlackBerry: function() {
         return navigator.userAgent.match(/BlackBerry/i);
     },
-    iOS: function () {
+    iOS: function() {
         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
-    Opera: function () {
+    Opera: function() {
         return navigator.userAgent.match(/Opera Mini/i);
     },
-    Windows: function () {
+    Windows: function() {
         return navigator.userAgent.match(/IEMobile/i);
     },
-    any: function () {
+    any: function() {
         return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
+            isMobile.Android() || 
+            isMobile.BlackBerry() || 
+            isMobile.iOS() || 
+            isMobile.Opera() || 
             isMobile.Windows());
     }
 };
 
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
-    let menuArrows =document.querySelectorAll('.menu__arrow');
-    if (menuArrows.length > 0){
-        for (let index = 0; index < menuArrows.length; index++) {
-            const menuArrow = menuArrows[index];
-            menuArrow.addEventListener("click", function(e) {
-                menuArrow.parentElement.classList.toggle('_active')
-            });
-        }
+if(isMobile.any() ) {
+  document.body.classList.add('_touch');
+
+  let menuArrows = document.querySelectorAll('.menu__arrowa');
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+        const menuArrow = menuArrows[index];
+        menuArrow.addEventListener("click", function (e) {
+            menuArrow.parentElement.classList.toggle('_active');
+        });
     }
+  }
+
 } else {
     document.body.classList.add('_pc');
+}
+
+
+const iconMenu = document.querySelector('.menu__icona');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__bodya');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
 }
 
 /*прокрутка при клике*/
@@ -51,7 +64,7 @@ if (menuLinkas.length > 0) {
         const menuLinka = e.target;
         if (menuLinka.dataset.goto && document.querySelector(menuLinka.dataset.goto)){
             const gotoBlock = document.querySelector(menuLinka.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClirntRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            const gotoBlockValue = gotoBlock.getBoundingClirntRect().top + pageYOffset - document.querySelector('headera').offsetHeight;
 
             window.scrollTo({
                 top: gotoBlockValue,
@@ -61,16 +74,7 @@ if (menuLinkas.length > 0) {
         }
     }
 }
-/*меню бургер*/
-const iconMenu = document.querySelector('.menu__icona');
-if (iconMenu){
-    const menuBody = document.querySelector('.menu__bodya');
-    iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}
+
 /*появление при скроле*/
 function onEntry(entry) {
     entry.forEach(change => {
