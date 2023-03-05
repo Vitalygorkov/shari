c8612ea01f4a5719015f1acff3b689b09e9792cc
Alenadecor <alenagorcova@gmail.com>
2023-03-05 20:54:38 +0600

    add category and balloon 05032023

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('baloon/', views.baloon, name='baloon'),
+    path('cart/', views.cart, name='cart'),
+    path('<category_slug>/', views.category, name='category'),
+    path('<category_slug>/<product_slug>/', views.show_product, name='product'),
     path('', views.index, name='index'),
 ]
