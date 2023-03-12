5e143d3234d1a90126154a0a5b2c2cef80966876
Alenadecor <alenagorcova@gmail.com>
2023-03-12 22:01:49 +0600

    12032023

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -16,39 +16,68 @@ def index(request):
     return HttpResponse(template.render( context, request))
 
 
-def baloon(request):
+# def baloon(request):
+#     products_list = Balloon.objects.all()
+#     categories = Category.objects.all()
+#     template = loader.get_template('shop/balloon.html')
+#     context = {
+#         'products_list': products_list,
+#         'categories': categories,
+#     }
+#     return HttpResponse(template.render( context, request))
+
+def cart(request):
     products_list = Balloon.objects.all()
     categories = Category.objects.all()
-    template = loader.get_template('shop/balloon.html')
+    template = loader.get_template('shop/cart.html')
     context = {
         'products_list': products_list,
         'categories': categories,
     }
     return HttpResponse(template.render( context, request))
 
-def cart(request):
+def favorites(request):
     products_list = Balloon.objects.all()
-    template = loader.get_template('shop/cart.html')
+    categories = Category.objects.all()
+    template = loader.get_template('shop/favorites.html')
     context = {
         'products_list': products_list,
+        'categories': categories,
     }
     return HttpResponse(template.render( context, request))
+
 
 def category(request, category_slug):
     # Category.objects.filter(url=category_slug)
     branch_categories = Category.objects.filter(url=category_slug).get_descendants(include_self=True)
+    print(branch_categories)
+    last_category = ''
+    try:
+        if category_slug:
+            print(category_slug)
+            last_category = Category.objects.get(url=category_slug)
+    except Exception as e:
+        print(e)
+        print('Ошибка поиска текущей категории ')
+
     products_list = Product.objects.filter(category__in=branch_categories)
+    print(products_list)
     categories = Category.objects.all()
-    template = loader.get_template('shop/category.html')
+    # template = loader.get_template('shop/category.html')
+    template_render = "shop/category.html"
     context = {
         'products_list': products_list,
+        "last_category": last_category,
         'categories': categories,
     }
-    return HttpResponse(template.render( context, request))
+    print(last_category)
+    return render(request, template_render, context)
 
-def show_product(request, product_slug):
+def show_product(request, product_slug, category_slug):
     categories = Category.objects.all()
-    product = Product.objects.get(slug=product_slug)
+    branch_categories = Category.objects.filter(url=category_slug).get_descendants(include_self=True)
+    # product = Product.objects.get(slug=product_slug)
+    last_category = Category.objects.get(url=category_slug)
     product_balloon = Balloon.objects.get(slug=product_slug)
     # prod1 = product.get_ancestors(asceding=True, include_self=False)
     template_render = "shop/baloon.html"
@@ -59,6 +88,6 @@ def show_product(request, product_slug):
     #     template_render = "shop/product.html"
     # print("show_product")
 
-    return render(request, template_render, {"product": product,
-                                            "product_balloon": product_balloon,
+    return render(request, template_render, {"product_balloon": product_balloon,
+                                             "last_category": last_category,
                                             'categories': categories, })
